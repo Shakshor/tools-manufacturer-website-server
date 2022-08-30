2a8fd2912f9a75690852d727483cce6eeccd1135
Shakshor <shakshor584@gmail.com>
2022-08-30 12:51:57 +0600

    structuring api again

--- index.js
+++ index.js
@@ -62,8 +62,7 @@ async function run() {
             }
         }
 
-
-
+        // ---------- product api ----------
         // load the product api
         app.get('/product', async (req, res) => {
             const query = {};
@@ -80,15 +79,60 @@ async function run() {
             res.send(product);
         });
 
+        // POST product
+        app.post('/product', verifyJWT, verifyAdmin, async (req, res) => {
+            const product = req.body;
+            const result = await toolsCollection.insertOne(product);
+            res.send(result);
+        })
 
+        // DELETE  specific product
+        app.delete('/product/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+        // ---------- all orders api ------------
         // load all orders
         app.get('/allOrders', verifyJWT, verifyAdmin, async (req, res) => {
             const query = {};
             const cursor = orderCollection.find(query);
             const allOrders = await cursor.toArray();
             res.send(allOrders);
-        })
+        });
 
+        // update specific order from all orders
+        app.put('/allOrders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedStatus = req.body;
+            // console.log(id, updatedStatus);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: updatedStatus.update,
+                }
+            }
+            const updatedOrder = await orderCollection.updateOne(filter, updatedDoc, options);
+            const updatedPayment = await paymentCollection.updateOne(filter, updatedDoc, options);
+            res.send(updatedOrder);
+        });
+
+        // DELETE  specific product
+        app.delete('/allOrders/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+        // ------------- orders api -----------------
         // load specific user orders
         app.get('/orders', verifyJWT, async (req, res) => {
             const user = req.query.user;
@@ -103,7 +147,7 @@ async function run() {
                 return res.status(403).send({ message: 'forbidden access' })
             }
 
-        })
+        });
 
         // load specific order
         app.get('/orders/:id', verifyJWT, async (req, res) => {
@@ -111,39 +155,51 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const order = await orderCollection.findOne(query);
             res.send(order);
-        })
+        });
 
+        // POST user orders
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        // update specific order
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId,
+                    status: payment.status,
+                }
+            }
+
+            const result = await paymentCollection.insertOne(payment);
+            const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+            res.send(updatedOrder);
+        });
+
+        // DELETE specific order
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+
+        // ----------- users api -------------
         // load all users
         app.get('/user', verifyJWT, async (req, res) => {
             // const user = req.body;
             const users = await userCollection.find().toArray();
             res.send(users);
-        })
-
-        // load or find user === admin ? true : false
-        app.get('/admin/:email', async (req, res) => {
-            const email = req.params.email;
-
-            // --- alternative process for find -----
-            // const query = {email: email};
-            // const user  =await userCollection.findOne(query);
-
-            const user = await userCollection.findOne({ email: email });
-            const isAdmin = user.role === 'admin'; // isAdmin is a boolean value
-            res.send({ admin: isAdmin });
-        })
-
-        // load all reviews
-        app.get('/review', async (req, res) => {
-            const query = {};
-            const cursor = reviewCollection.find(query);
-            const reviews = await cursor.toArray();
-            res.send(reviews);
-        })
-
-
-
-
+        });
 
         // update user role as admin(If login user is admin,then make selected user an admin)
         app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
@@ -173,47 +229,46 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc, options);
             const newToken = jwt.sign({ email: email }, process.env.DB_TOKEN_SECRET, { expiresIn: '24d' })
             res.send({ result, newToken });
-        })
-
-        // update specific order
-        app.patch('/orders/:id', async (req, res) => {
-            const id = req.params.id;
-            const payment = req.body;
-            const filter = { _id: ObjectId(id) };
-            const updatedDoc = {
-                $set: {
-                    paid: true,
-                    transactionId: payment.transactionId,
-                    status: payment.status,
-                }
-            }
-
-            const result = await paymentCollection.insertOne(payment);
-            const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
-            res.send(updatedOrder);
         });
 
-        // update specific order from all orders
-        app.put('/allOrders/:id', async (req, res) => {
-            const id = req.params.id;
-            const updatedStatus = req.body;
-            // console.log(id, updatedStatus);
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    status: updatedStatus.update,
-                }
-            }
-            const updatedOrder = await orderCollection.updateOne(filter, updatedDoc, options);
-            const updatedPayment = await paymentCollection.updateOne(filter, updatedDoc, options);
-            res.send(updatedOrder);
+
+        // load or find user === admin ? true : false
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+
+            // --- alternative process for find -----
+            // const query = {email: email};
+            // const user  =await userCollection.findOne(query);
+
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin'; // isAdmin is a boolean value
+            res.send({ admin: isAdmin });
+        });
+
+
+
+
+        // ----------------- review api -----------
+        // load all reviews
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        // POST review
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            // console.log(review);
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         })
 
 
 
 
-
+        // -------------- create payment api ---------------
         // paymentIntent POST api
         app.post('/create-payment-intent', verifyJWT, async (req, res) => {
             // const {price} = req.body; // directly destructure  
@@ -231,58 +286,6 @@ async function run() {
             });
             res.send({ clientSecret: paymentIntent.client_secret })
         })
-
-
-
-        // POST product
-        app.post('/product', verifyJWT, verifyAdmin, async (req, res) => {
-            const product = req.body;
-            const result = await toolsCollection.insertOne(product);
-            res.send(result);
-        })
-
-        // POST user orders
-        app.post('/orders', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
-        })
-
-        // POST review
-        app.post('/review', async (req, res) => {
-            const review = req.body;
-            // console.log(review);
-            const result = await reviewCollection.insertOne(review);
-            res.send(result);
-        })
-
-
-
-
-        // DELETE specific order
-        app.delete('/orders/:id', verifyJWT, async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
-            res.send(result);
-        });
-
-        // DELETE  specific product
-        app.delete('/allOrders/:id', verifyJWT, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
-            res.send(result);
-        });
-
-
-        // DELETE  specific product
-        app.delete('/product/:id', verifyJWT, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await toolsCollection.deleteOne(query);
-            res.send(result);
-        });
 
     }
     finally {
